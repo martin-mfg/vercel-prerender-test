ddcd97d0890c63987d5a86aaa2626164ba793b3b
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2025-06-22 19:15:25 +0000

    log original request

--- .vercel/output/functions/blog/endpoint.func/index.mjs
+++ .vercel/output/functions/blog/endpoint.func/index.mjs
@@ -1,6 +1,6 @@
 import { default as handler } from './pin.mjs'
 
 export default async (req, res) => {
-  console.log('Request received:', req.url)
+  console.log('Request received:', req.url, " original route:" + req.headers['x-now-route-matches'])
   handler(req, res)
 }
