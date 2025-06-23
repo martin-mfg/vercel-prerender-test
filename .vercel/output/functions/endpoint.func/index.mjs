2e75d3afaa09c56833a6f934fd1af9fc21f3b168
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2025-06-23 07:00:49 +0000

    parse header instead

--- .vercel/output/functions/endpoint.func/index.mjs
+++ .vercel/output/functions/endpoint.func/index.mjs
@@ -3,7 +3,7 @@ import { default as handler } from './pin.mjs'
 
 export default async (req, res) => {
   console.log('Request received:', req.url, "query:", req.query, " original route:", req.headers['x-now-route-matches'])
-  const matches = parse(req.url)
+  const matches = parse(req.headers['x-now-route-matches'])
   console.log('Parsed query:', matches)
   handler(req, res)
 }
