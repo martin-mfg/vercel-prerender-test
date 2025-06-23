cc5bbdd66aa93e98d448c3f48cb2028f8a82a59f
martin-mfg <2026226+martin-mfg@users.noreply.github.com>
2025-06-23 06:53:15 +0000

    try parsing query

--- .vercel/output/functions/endpoint.func/index.mjs
+++ .vercel/output/functions/endpoint.func/index.mjs
@@ -1,6 +1,9 @@
+import parse from 'querystring'
 import { default as handler } from './pin.mjs'
 
 export default async (req, res) => {
   console.log('Request received:', req.url, "query:", req.query, " original route:", req.headers['x-now-route-matches'])
+  const matches = parse(req.url)
+  console.log('Parsed query:', matches)
   handler(req, res)
 }
